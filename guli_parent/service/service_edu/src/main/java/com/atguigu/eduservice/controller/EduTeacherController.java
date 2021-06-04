e92622b8414e004b00e2b233a6e49ae9757fa231
Hawaii <414097899@qq.com>
2021-06-04 20:55:52 +0800

    新增教师查询接口

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,17 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.api.R;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
+    //访问地址： http://localhost:8001/eduservice/teacher/findAll
+    //把service注入
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //1 查询讲师表所有数据
+    //rest风格
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public List findAllTeacher() {
+        //调用service的方法实现查询所有的操作
+        List<EduTeacher> list = teacherService.list(null);
+        return list;
+    }
 
 }
 
