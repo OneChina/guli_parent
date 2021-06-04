4acd5be568ba437f0b491ada1a73ece11b6c7be2
Hawaii <414097899@qq.com>
2021-06-04 21:47:23 +0800

    新增swagger接口

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -3,13 +3,10 @@ package com.atguigu.eduservice.controller;
 
 import com.atguigu.eduservice.entity.EduTeacher;
 import com.atguigu.eduservice.service.EduTeacherService;
-import com.baomidou.mybatisplus.extension.api.R;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +34,15 @@ public class EduTeacherController {
         //调用service的方法实现查询所有的操作
         List<EduTeacher> list = teacherService.list(null);
         return list;
+    }
+
+    //2 逻辑删除讲师的方法
+    @ApiOperation(value = "逻辑删除讲师")
+    @DeleteMapping("{id}")
+    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
+                           @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        return flag;
     }
 
 }
